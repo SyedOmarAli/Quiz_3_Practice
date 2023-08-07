a90256b2f0d6fe53106d239b94d39c2d9454f3cd
SyedOmarAli <syedomarali.smit@gmail.com>
2023-08-07 20:27:11 +0500

    updated

---
+++ app.js
@@ -0,0 +1,112 @@
+// var x = 10;
+// function foo() {
+//     console.log(x);
+//     var x = 20;
+
+// }
+// foo();
+
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(function() {
+//         console.log(i);
+//     }, 1000)
+// }
+
+// var x = 10;
+// if (true) {
+
+//     var x = 20;
+//     console.log(x);
+
+// }
+// console.log(x);
+
+// console.log(typeof null);
+
+// console.log(1 + "2" + 3);
+
+// var x = 10;
+// function foo() {
+//     console.log(x);
+// }
+//  foo();
+// var x = 20;
+
+// var x = 5;
+// function changeValue(x) {
+//     var x = 10;
+   
+// }
+// changeValue(x);
+// console.log(x);
+
+// var x = 5;
+// function foo() {
+//     console.log(x);
+//     var x = 10;
+//     console.log(x);
+//   }
+
+// foo();
+
+// var add = (function() {
+//     var counter = 0;
+//     return function() {
+//         return counter += 1;
+//     }
+// })();
+// console.log(add());
+// console.log(add());
+// console.log(add());
+
+// let x = [] instanceof Array;
+// console.log(x);
+
+// console.log(typeof []);
+
+// console.log(2 ** 4);
+
+// console.log(null == undefined);
+
+// console.log(true);
+
+// console.log(typeof NaN);
+
+// var x = 5;
+// function outer() {
+//     var x = 10;
+//     function inner() {
+//         console.log(x);
+//     }
+//     return inner;
+// }
+// var innerFn = outer();
+// innerFn();
+
+// console.log([] instanceof Object);
+
+// var x = 5;
+//  x = "Hello";
+// console.log(typeof x);
+
+// console.log(typeof typeof typeof 1);
+
+// console.log(Boolean([]));
+
+// var x = 5;
+// function foo() {
+//     console.log(x);
+//     x = 10;
+//     console.log(x);
+// }
+// foo();
+
+// console.log("Hello" * 2);
+
+// var x = 5;
+// function foo() {
+//     console.log(x);
+//     var x = 10;
+//     console.log(x);
+// }
+// foo();
